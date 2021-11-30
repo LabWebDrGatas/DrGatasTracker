7f9ea143e3cf361a4ad43a5df48eb077d155ed57
Trevinno <josericardo.trdz@gmail.com>
2021-11-30 01:35:45 -0600

    Delete and login

--- frontend/src/component/tableItem.jsx
+++ frontend/src/component/tableItem.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 import { useHistory } from "react-router-dom";
 
+import axios from "axios";
+
 import "../css/table.css";
+
+import { apiAddress } from "../connections";
 
 const TableItem = ({ numero_de_rastreo, cliente, correo, buzon, estado, marca, modelo, material_de_suela, servicio_extra }) => {
   let history = useHistory();
   let actualizarPedido = () => {
-    console.log('this is the edit button')
+    console.log('edit button')
     console.log('/Update/' + numero_de_rastreo)
     history.push('/Update/' + numero_de_rastreo);
   };
+  
+  let url = `${apiAddress}/deletePedido/`;
 
-  let borrarPedido = () => {
+  let borrarPedido = async () => {
     console.log('this is the delete button')
+    console.log(url + numero_de_rastreo)
+    let pedido_borrado = await axios.delete(url + numero_de_rastreo, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token")
+    },
+    })
+    console.log(pedido_borrado.data, 'Delete Pedido')
+    window.location.reload(false);
   };
 
   return (
